a67fcfca9f4ecce64b2eff0175d242eba7abd055
isabel k. lee <hello@isabelklee.co>
2020-02-21 16:51:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -44,7 +44,7 @@ def get_english_meaning(emoticons, japanese_emo)
       if japanese_emo == key
         meaning = value
       end
-  }
+    }
 
     if get_emoticon.include? japanese_emo = false
       meaning = "Sorry, that emoticon was not found"
